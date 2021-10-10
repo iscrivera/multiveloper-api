a0a8c12d76903afb9301fef0d3193e8731d3c6b4
GFSSCORP\C262780 <asrivera@santander.com.mx>
2021-10-09 23:22:37 -0500

    add dekorate

--- src/main/java/mx/multivelopers/api/ApiRestApplication.java
+++ src/main/java/mx/multivelopers/api/ApiRestApplication.java
@@ -3,7 +3,11 @@ package mx.multivelopers.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.kubernetes.annotation.Port;
+import io.dekorate.openshift.annotation.OpenshiftApplication;
+
 @SpringBootApplication
+@OpenshiftApplication(name = "multiveloper-api", ports = @Port(name = "web", containerPort = 8080), expose = true)
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
